a7afed38a2013d783a04f4f0b4bc4465c487d575
Michael Muniu <muniu.dev@gmail.com>
2024-02-11 14:32:12 +0300

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == self.sorted_word]
+    
+    def __init__(self, word):
+        self.sorted_word = sorted([letter for letter in word])
+        print(self.sorted_word)
